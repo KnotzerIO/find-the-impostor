e9845d3ea4fdba15e6da7bd7945b75b0200ac6a9
Mark <knotzermark@gmail.com>
2025-06-16 12:32:18 +0200

    feat: add Umami analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        async
+        src={process.env.UMAMI_SCRIPT_URL}
+        data-website-id={process.env.UMAMI_WEBSITE_ID}
+      />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
